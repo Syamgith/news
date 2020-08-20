1a162e38919e11cf3ba0201d6846b2587680b6e8
syamgith <syamgithksudheesh@gmail.com>
2020-08-20 17:32:43 +0530

    newsapiprovider fetched data

--- lib/src/resources/news_api_provider.dart
+++ lib/src/resources/news_api_provider.dart
@@ -1,10 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' show Client;
+import 'package:news/src/models/item_model.dart';
 
 class NewsApiProvider {
   Client client = Client();
-  fetchTopIds() {
-    client.get('https://hacker-news.firebaseio.com/v0/topstories.json');
+
+  fetchTopIds() async {
+    final response = await client
+        .get('https://hacker-news.firebaseio.com/v0/topstories.json');
+    final ids = json.decode(response.body);
+    return ids;
   }
 
-  fetchItem() {}
+  fetchItem(int id) async {
+    final response =
+        await client.get('https://hacker-news.firebaseio.com/v0/user/$id.json');
+    final parsedJson = json.decode(response.body);
+    return ItemModel.fromJson(parsedJson);
+  }
 }
