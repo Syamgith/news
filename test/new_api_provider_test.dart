6864319b04ce433ca33dc66af0cdccd8c5b84f4d
syamgith <syamgithksudheesh@gmail.com>
2020-08-20 20:37:05 +0530

    adding test

---
+++ test/new_api_provider_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test(
+    'fetch top ids returns ids',
+    () {
+      int sum = 1 + 3;
+      expect(sum, 4);
+    },
+  );
+}
