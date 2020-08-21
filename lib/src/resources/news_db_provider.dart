0069a9fe9863a969070c9ad0d2feaeb3c30218e4
syamgith <syamgithksudheesh@gmail.com>
2020-08-21 15:04:14 +0530

    db created

--- lib/src/resources/news_db_provider.dart
+++ lib/src/resources/news_db_provider.dart
@@ -1,6 +1,37 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class NewsDbProvider {
   Database db;
-  init() async {}
+  init() async {
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, "items.db");
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+        CREATE TABLE Items
+        (
+          id INTEGER PRIMARY KEY,
+          type TEXT,
+          by TEXT,
+          time INTEGER,
+          text TEXT,
+          parent INTEGER,
+          kids BLOB,
+          dead INTEGER,
+          deleted INTEGER,
+          url TEXT,
+          score INTEGER,
+          title TEXT,
+          descendants INTEGER,
+        )
+        """);
+      },
+    );
+  }
 }
