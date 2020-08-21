cffd73cb6346240c50f524e3df1f72bcee5bbbb2
syamgith <syamgithksudheesh@gmail.com>
2020-08-21 15:45:40 +0530

    repository.dart created

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,15 @@
+
+import 'package:news/src/resources/news_api_provider.dart';
+import 'package:news/src/resources/news_db_provider.dart';
+
+class Repository {
+  NewsApiProvider apiProvider = NewsApiProvider();
+  NewsDbProvider dbProvider = NewsDbProvider();
+
+  fetchTopIds() {
+
+  }
+  fetchItem() {
+
+  }
+}
