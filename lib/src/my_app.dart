029b243e0c64993342fd58e71ca85cee6bdffb2c
syamgith <syamgithksudheesh@gmail.com>
2020-09-05 13:52:32 +0530

    routes made

--- lib/src/my_app.dart
+++ lib/src/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/blocs/stories_provider.dart';
+import 'package:news/src/screens/news_details.dart';
 import 'package:news/src/screens/news_list.dart';
 
 class MyApp extends StatelessWidget {
@@ -8,14 +9,24 @@ class MyApp extends StatelessWidget {
     return StoriesProvider(
       child: MaterialApp(
         title: 'News',
-        onGenerateRoute: (RouteSettings settings) {
-          return MaterialPageRoute(
-            builder: (context) {
-              return NewsList();
-            },
-          );
-        },
+        onGenerateRoute: routes,
       ),
     );
+  }
+
+  Route routes(RouteSettings settings) {
+    if (settings.name == '/') {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsList();
+        },
+      );
+    } else {
+      return MaterialPageRoute(
+        builder: (context) {
+          return NewsDetails();
+        },
+      );
+    }
   }
 }
