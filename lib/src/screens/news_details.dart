4e0ac7b0844b4c06c997bb39bcf0a11c7496901f
syamgith <syamgithksudheesh@gmail.com>
2020-09-10 17:01:36 +0530

    news details started

--- lib/src/screens/news_details.dart
+++ lib/src/screens/news_details.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/comments_provider.dart';
 
 class NewsDetails extends StatelessWidget {
   final itemId;
   NewsDetails({this.itemId});
   @override
   Widget build(BuildContext context) {
+    final bloc = CommentsProvider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Details'),
       ),
-      body: Text('$itemId details'),
+      body: buildBody(bloc),
     );
   }
+
+  buildBody(CommentsBloc bloc) {}
 }
