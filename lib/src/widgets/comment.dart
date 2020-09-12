38bb37220b90e252dfa61c850dc30384760bd2e0
syamgith <syamgithksudheesh@gmail.com>
2020-09-12 15:41:23 +0530

    added loading container

--- lib/src/widgets/comment.dart
+++ lib/src/widgets/comment.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/models/item_model.dart';
+import 'package:news/src/widgets/loading_container.dart';
 
 class Comment extends StatelessWidget {
   final int itemId;
@@ -36,7 +37,7 @@ class Comment extends StatelessWidget {
             children: children,
           );
         }
-        return Text('Comment still Loading');
+        return LoadingContainer();
       },
     );
   }
